c2ced53a733dc94d1eccd9f306d4d4a9604d2f55
Tatiana Viana <tatisabinoviana@gmail.com>
2021-10-01 10:25:49 -0300

    criacao do photos-list pelo ng generate e retirada algumas responsabilidades excedentes do module e html principal

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,15 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 
 @NgModule({
-  declarations: [ PhotoComponent ],
-  exports: [ PhotoComponent ],
+  declarations: [ 
+    PhotoComponent, 
+    PhotoListComponent 
+  ],
   imports: [ HttpClientModule ]
 })
 export class PhotosModule {}
