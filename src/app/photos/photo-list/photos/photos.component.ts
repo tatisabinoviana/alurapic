28a95f9402904a49886b66748e64f529acbf2aa6
Tatiana Viana <tatisabinoviana@gmail.com>
2021-10-12 09:54:16 -0300

    subindo alterações

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -11,9 +11,23 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PhotosComponent implements OnInit {
 
   @Input() photos: Photo[] = [];
+  rows: any[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.rows = this.groupColumns(this.photos);
+  }
+
+  groupColumns(photos: Photo[]){
+    const newRows = [];
+
+    for(let index = 0; index < photos.length; index += 3){
+      newRows.push(photos.slice(index, index + 3
+        ));
+    }
+
+    return newRows;
   }
 
 }
