f8ece87925221c7727dbf7ee4a5822ccc6999cc7
Tatiana Viana <tatisabinoviana@gmail.com>
2021-10-14 22:01:18 -0300

    desenvolvido o revolver de photo-list, impementado o debounceTime para limitar o carregamento automatico, implementado o ngOnDestroy quebrando a memoria desnecessaria apos uso

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,14 +1,27 @@
-import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from '@angular/router';
 
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 
 const routes: Routes = [
-  { path: 'user/:userName', component: PhotoListComponent },
-  { path: 'p/add', component: PhotoFormComponent },
-  { path: '**', component: NotFoundComponent },
+  { 
+    path: 'user/:userName', 
+    component: PhotoListComponent, 
+    resolve:  {
+      photos: PhotoListResolver
+    }
+  },
+  { 
+    path: 'p/add', 
+    component: PhotoFormComponent 
+  },
+  { 
+    path: '**', 
+    component: NotFoundComponent 
+  },
 ];
 
 @NgModule ({
