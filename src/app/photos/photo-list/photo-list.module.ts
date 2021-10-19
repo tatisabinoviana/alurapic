9ca7f2eafad3eca4f48a9b9b0e0bf14042f10fd1
Tatiana Viana <tatisabinoviana@gmail.com>
2021-10-18 21:31:38 -0300

    criacao de modules para as ramificações do codigo

---
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,23 @@
+import { PhotoModule } from './../photo/photo.module';
+import { FilterByDescription } from './filter-by-description.pipe';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { PhotosComponent } from './photos/photos.component';
+import { PhotoListComponent } from './photo-list.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    PhotoListComponent, 
+    PhotosComponent,
+    LoadButtonComponent,
+    FilterByDescription
+  ],
+  imports: [
+    CommonModule,
+    PhotoModule
+  ]
+})
+export class PhotoListModule {
+
+}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,27 +1,14 @@
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoModule } from './photo/photo.module';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 
 @NgModule({
-  declarations: [ 
-    PhotoComponent, 
-    PhotoListComponent, 
-    PhotoFormComponent, 
-    PhotosComponent,
-    FilterByDescription,
-    LoadButtonComponent
-  ],
   imports: [
-    HttpClientModule, 
-    CommonModule
+    PhotoModule,
+    PhotoFormModule,
+    PhotoListModule
   ]
 })
 export class PhotosModule {}
