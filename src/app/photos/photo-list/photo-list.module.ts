8a57a8a4386d9ddc1b377ffd93f7b39e8094ecbd
Tatiana Viana <tatisabinoviana@gmail.com>
2021-10-19 22:32:09 -0300

    criacao do componente Search para a distribuicao de rsponsabilidades do photo-list

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,3 +1,4 @@
+import { SearchComponent } from './search/search.component';
 import { CardModule } from './../../shared/components/card/card.module';
 import { PhotoModule } from './../photo/photo.module';
 import { FilterByDescription } from './filter-by-description.pipe';
@@ -12,7 +13,8 @@ import { CommonModule } from '@angular/common';
     PhotoListComponent, 
     PhotosComponent,
     LoadButtonComponent,
-    FilterByDescription
+    FilterByDescription, 
+    SearchComponent
   ],
   imports: [
     CommonModule,
