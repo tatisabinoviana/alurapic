392052d603591e0f1d6750aa54abc2de659748cc
Tatiana Viana <tatisabinoviana@gmail.com>
2021-11-03 20:26:03 -0300

    criação do auth service e dos vmessage para a reutilização de codigo

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,17 @@
+import { RouterModule } from '@angular/router';
+import { VMessageModule } from './../shared/components/vmessage/vmessage.module';
+import { CommonModule } from '@angular/common';
 import { SignInComponent } from './signin/signin.component';
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations:[ SignInComponent ], 
-  imports: [ ReactiveFormsModule ]
+  imports: [ 
+    CommonModule, 
+    ReactiveFormsModule, 
+    VMessageModule, 
+    RouterModule
+  ]
 })
 export class HomeModule { }
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,7 @@
+import { AuthService } from './../../core/auth.service';
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './signin.component.html'
@@ -8,12 +10,33 @@ export class SignInComponent implements OnInit {
   
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder, 
+    private authService: AuthService, 
+    private router: Router
+  ) {}
   
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  login(){
+    
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService
+      .authenticate(userName, password)
+      .subscribe(
+        () => this.router.navigate(['user', userName]), 
+        err => {
+          console.log(err);
+          this.loginForm.reset();
+          alert('Invalid user name or password');
+        }
+      );
   }
 }
