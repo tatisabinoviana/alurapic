e728b01357d18955303b6cd84d23728cda0003c9
Tatiana Viana <tatisabinoviana@gmail.com>
2021-11-19 11:20:33 -0300

    Melhoria de performance do router no html do header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from "@angular/core";
@@ -5,6 +6,9 @@ import { NgModule } from "@angular/core";
 @NgModule({
   declarations: [ HeaderComponent ], 
   exports: [ HeaderComponent ],
-  imports: [CommonModule]
+  imports: [
+    CommonModule, 
+    RouterModule
+  ]
 })
 export class CoreModule {}
