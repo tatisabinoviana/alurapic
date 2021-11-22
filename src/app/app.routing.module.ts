4d2100b4d6204dffd560033a58deede256ec703c
Tatiana Viana <tatisabinoviana@gmail.com>
2021-11-22 11:47:31 -0300

    adicionada rota para registro de usuario, seu html, ts e declaration no module principal

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,3 +1,4 @@
+import { SignUpComponent } from './home/signup/signup.component';
 import { AuthGuard } from './core/auth/auth.guard';
 import { SignInComponent } from './home/signin/signin.component';
 import { NgModule } from "@angular/core";
@@ -13,6 +14,10 @@ const routes: Routes = [
     path: '', 
     component: SignInComponent,
     canActivate: [AuthGuard] 
+  },
+  { 
+    path: 'signup', 
+    component: SignUpComponent
   },
   { 
     path: 'user/:userName', 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,19 @@
+import { SignUpComponent } from './signup/signup.component';
 import { RouterModule } from '@angular/router';
 import { VMessageModule } from './../shared/components/vmessage/vmessage.module';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './signin/signin.component';
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations:[ SignInComponent ], 
+  declarations:[ 
+    SignInComponent, 
+    SignUpComponent
+  ], 
   imports: [ 
     CommonModule, 
+    FormsModule, 
     ReactiveFormsModule, 
     VMessageModule, 
     RouterModule
